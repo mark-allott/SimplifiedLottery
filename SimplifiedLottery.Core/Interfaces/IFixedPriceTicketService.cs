f2cf9e4f72c321a1514395ed79312fd8cdc7a01f
Mark Allott <47857898+mark-allott@users.noreply.github.com>
2025-11-24 10:58:17 +0000

    Define an interface for a ticket service that provides tickets where they all cost the same amount

---
+++ SimplifiedLottery.Core/Interfaces/IFixedPriceTicketService.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+
+namespace SimplifiedLottery.Core.Interfaces
+{
+	public interface IFixedPriceTicketService<out T, out TT>
+		where T : ITicket<TT>
+		where TT : struct
+	{
+		/// <summary>
+		/// Buy a single ticket from the service
+		/// </summary>
+		/// <returns>The ticket bought from the service</returns>
+		T BuyTicket();
+
+		/// <summary>
+		/// Buys the specified number of tickets from the service
+		/// </summary>
+		/// <param name="count">The total number of tickets to buy</param>
+		/// <returns>The tickets bought</returns>
+		IEnumerable<T> BuyTickets(int count);
+
+		/// <summary>
+		/// Provides access to the tickets bought through the service
+		/// </summary>
+		IEnumerable<T> Tickets { get; }
+	}
+}
